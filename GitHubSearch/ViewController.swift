5c492539ac344f8e8641b495400a8f4ff26bb482
Diego Manuel Molina Canedo <diego-molicane@hotmail.com>
2017-05-27 20:06:43 +0200

    First Commit

---
+++ GitHubSearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHubSearch
+//
+//  Created by Diego Manuel Molina Canedo on 27/5/17.
+//  Copyright © 2017 Universidad Pontificia de Salamanca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
